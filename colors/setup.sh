d7bdb30fe37cd9b81bbc319ffcee3ba1ed03a35d
jsaitou <jsaitou@mail.zieera.com>
2012-10-28 23:33:44 +0900

    change setup

--- colors/setup.sh
+++ colors/setup.sh
@@ -4,8 +4,11 @@
 echo "Setup colors environment......."
 mv ~/.dir_colors ~/.dir_colors.save
 ln -s `pwd`/dir_colors ~/.dir_colors
-echo "export PATH=\"\$(brew --prefix coreutils)/libexec/gnubin:\$PATH\"" >> ~/.bashrc  
+#if MacOX, inline below .
+#echo "export PATH=\"\$(brew --prefix coreutils)/libexec/gnubin:\$PATH\"" >> ~/.bashrc  
 echo "eval \`dircolors ~/.dir_colors -b\`" >> ~/.bashrc 
-source ~/.bashrc
-echo "Done setup."
+
+echo "Done setup. Please 'source ~/.bashrc'"
+
+
 
