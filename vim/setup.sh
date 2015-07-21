7c7166c843ec2f84316150d27deff13752233c1a
jorira <jorira.no1@gmail.com>
2015-07-21 10:09:49 +0900

    NeoBundleに変更

--- vim/setup.sh
+++ vim/setup.sh
@@ -1,11 +1,18 @@
 #! /bin/sh
 
-# vim 
+# Vim NeoBundle Setup
 echo "Setup vim environment......."
-git clone https://github.com/gmarik/vundle.git vim.d/bundle/vundle
+git clone https://github.com/Shougo/neobundle.vim.git vim.d/bundle/neobundle.vim
+
+# Make backup
 mv ~/.vimrc ~/.vimrc.save
+
 ln -s `pwd`/vimrc ~/.vimrc
+
+# Make backup
 mv ~/.vim  ~/.vim.save
+
 ln -s `pwd`/vim.d ~/.vim
-echo "Done. Please exec Vim command ':BundleInstall' "
+
+echo "Done. Please exec Vim command ':NeoBundleInstall' "
 
