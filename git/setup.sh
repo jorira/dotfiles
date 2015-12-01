280809e70842890b374c2e646a88950d0eb504ee
jorira <jorira.no1@gmail.com>
2015-12-01 12:07:16 +0900

    Update setup.sh

--- git/setup.sh
+++ git/setup.sh
@@ -18,7 +18,7 @@ fi
 ln -s `pwd`/gitconfig $CONFIG_PATH
 ln -s `pwd`/gitignore $IGNORE_PATH
 
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -O ~/.git-completion.bash
 echo "source ~/.git-completion.bash" >> ~/.bashrc
 
 echo "Done. Setup git environment."
