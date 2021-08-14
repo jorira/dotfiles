08604ae71d3c1b28811b7aaa7e85f022d36eb1dc
/any/ </any/>
2021-08-14 22:19:54 +0900

    Add vimrc for vim8 with dein.vim
    Use dein.vim as plugin maneger.

---
+++ vim8/setup.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Vim NeoBundle Setup
+echo "Setup vim environment......."
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh installer.sh ~/.cache/dein
+
+# Make backup
+if [ -f ~/.vimrc ]; then
+  mv ~/.vimrc ~/.vimrc.save
+fi
+
+ln -s `pwd`/vimrc ~/.vimrc
+
+# when gvim
+# ln -s `pwd`/gvimrc ~/.gvimrc
+
+vim +":call dein#install()" +:q
+echo "Done. vim setup. "
+
